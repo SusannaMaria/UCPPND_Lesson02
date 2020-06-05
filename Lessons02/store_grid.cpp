ba4dee551325d036cbe557d57ab17114540f14c6
Susanna Maria <susanna@olsoni.de>
2020-06-05 13:59:04 +0200

    storegrid

---
+++ Lessons02/store_grid.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using std::cout;
+using std::vector;
+
+int main() {
+  // TODO: Declare a "board" variable here, and store
+  // the data provided above.
+
+  vector<vector<int>> board = {{0, 1, 0, 0, 0, 0},
+                               {0, 1, 0, 0, 0, 0},
+                               {0, 1, 0, 0, 0, 0},
+                               {0, 1, 0, 0, 0, 0},
+                               {0, 0, 0, 0, 1, 0}};
+
+    for(auto row : board){
+        for(auto cell : row){
+            cout << cell << " ";
+        }
+        cout << "\n";
+    }
+
+}
---
+++ jupyter_notebook_c++/README.md
@@ -0,0 +1,20 @@
+# Environment to use Jupyter notebooks for C++
+
+## Installation
+[xeus](https://github.com/jupyter-xeus/xeus)
+
+```
+    conda create -n cppnd
+    conda activate cppnd
+    conda install xeus -c conda-forge
+    conda install xeus-cling -c conda-forge
+    conda install notebook -c conda-forge
+```
+
+## Usage
+[xeus cling](https://github.com/jupyter-xeus/xeus-cling)
+
+```
+    conda activate cppnd
+    jupyter notebook
+```
